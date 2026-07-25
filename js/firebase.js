242357075357b527dd6b81dab3f0eef30ce50326
BRTFC <russ2178@gmail.com>
2026-07-25 06:09:29 +0100

    Update firebase.js

--- js/firebase.js
+++ js/firebase.js
@@ -2,6 +2,13 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-app.js";
 import { getDatabase, ref, set, get, push, update, remove, onValue, child }
   from "https://www.gstatic.com/firebasejs/10.12.0/firebase-database.js";
+import {
+  getAuth,
+  signInWithEmailAndPassword,
+  signOut,
+  sendPasswordResetEmail,
+  onAuthStateChanged
+} from "https://www.gstatic.com/firebasejs/10.12.0/firebase-auth.js";
 
 const firebaseConfig = {
   apiKey: "AIzaSyAvjoLg50J-V2PvUAEfSYFRQLhc9Ld_O9I",
@@ -14,7 +21,9 @@ const firebaseConfig = {
   measurementId: "G-TX97R668W8"
 };
 
-const app = initializeApp(firebaseConfig);
-const db  = getDatabase(app);
+const app  = initializeApp(firebaseConfig);
+const db   = getDatabase(app);
+const auth = getAuth(app);
 
-export { db, ref, set, get, push, update, remove, onValue, child };
+export { db, auth, ref, set, get, push, update, remove, onValue, child,
+         signInWithEmailAndPassword, signOut, sendPasswordResetEmail, onAuthStateChanged };
